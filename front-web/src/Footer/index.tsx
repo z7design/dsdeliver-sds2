714a54b7e40260831c5eeba22e51a6ca708b19d0
josemario <z7design@z7design.com.br>
2021-01-07 13:16:29 -0300

    Add routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,13 +10,13 @@ function Footer(){
        <footer className="main-footer">
            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
            <div className="footer-icons">
-                    <a href="#" target="_new">
+                    <a href="https:z7design.com.br" target="_new">
                         <YouTubeIcon />
                     </a>
-                    <a href="#" target="_new">
+                    <a href="https:z7design.com.br" target="_new">
                         <LinkedinIcon />
                     </a>
-                    <a href="#" target="_new">
+                    <a href="https:z7design.com.br" target="_new">
                         <InstagramIcon />
                     </a>
            </div>
